eca35a8677e251725708dc5ab1721a6986368139
mustafassal <mustafa.assal10@gmail.com>
2022-11-28 17:57:45 -0800

    shows strikethrough when task is checked; made add button shorter

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ function App() {
     setInput(e.target.value);
   };
 
+  const [checked, setChecked] = useState([]);
+
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  const isChecked = (item) =>
+    checked.includes(item) ? "checked-item" : "not-checked-item";
+
   const addTodo = (todo) => {
     const newTodo = {
       id: Math.random(),
@@ -33,8 +48,8 @@ function App() {
       <button onClick={() => addTodo(input)}>Add</button>
       {list.map((todo) => (
         <div key={todo.id}>
-          <input type="checkbox" value={todo.todo} />
-          <span>{todo.todo}</span>
+          <input type="checkbox" value={todo.todo} onChange={handleCheck} />
+          <span className={isChecked(todo.todo)}>{todo.todo}</span>
         </div>
       ))}
     </div>
