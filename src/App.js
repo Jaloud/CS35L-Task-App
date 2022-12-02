e4ca0458559eebbca74881be1f0645b9a4286f82
Ammar999aa <ammar1@g.ucla.edu>
2022-12-02 00:57:14 -0800

    implemented search bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "./components/header.js";
 import TaskList from "./components/TaskList.js";
+import SearchBar from "./components/searchBar.js";
 import "./index.css";
 
 function App() {
+  
+  const [inputText, setInputText] = useState("");
+
+  const handleChange2 = (event) => {
+    setInputText(event.target.value); // set e to text entered
+  };
+  
+  const focus = useRef(null);
+  
+  useEffect(() => {
+    focus.current.focus();
+  });
+
   return (
     <div className="App">
       <Header />
       <div className="tasks">
         <TaskList />
+    <h1 className="searbarHeader">Search</h1>
+      <div className="s-form">
+        <input class="s-input"
+          type="text"
+          value={inputText}
+          onChange={handleChange2}
+          ref={focus}
+        />
       </div>
+      <SearchBar input={inputText} />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/searchBar.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBhLLd4MOWrS9xXVjp_GGhZ6TCKEVOUhpk",
+  authDomain: "cs35l-task-app.firebaseapp.com",
+  projectId: "cs35l-task-app",
+  storageBucket: "cs35l-task-app.appspot.com",
+  messagingSenderId: "875742275629",
+  appId: "1:875742275629:web:de8cc453b17201e7d070a4",
+  measurementId: "G-6E45S5H59J",
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+function SearchBar(input) {
+  const [taskData, setTaskData] = useState([]);
+
+  useEffect(() => {
+    db.collection("tasks").onSnapshot((snapshot) => {
+      setTaskData(
+        snapshot.docs.map((doc) => ({
+          data: doc.data(),
+          id: doc.id,
+        }))
+      );
+    });
+  }, []);
+
+  const taskname = (par) => {
+    const data = par.data;
+    return data.taskname;
+  };
+  const getNotes = (par) => {
+    const data = par.data;
+    return data.notes;
+  };
+
+  let sortedTasks = taskData.sort((a, b) => b.important - a.important); // sort the important Tasks and move to top
+  const filtered = sortedTasks.filter((entry) => {
+    const newInput = input.input.toLowerCase();
+    var tsName = taskname(entry);
+    tsName = tsName.toLowerCase();
+    var ntes = getNotes(entry);
+    // ntes = ntes.toLowerCase();
+    return (tsName.includes(newInput) || 
+            ntes.includes(newInput)      
+    );
+  });
+  return (
+    <div>
+      <ul>
+        {filtered.map((item) => (
+          <li>{taskname(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchBar;
